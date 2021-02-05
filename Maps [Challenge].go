3f7ff3681c1715b1fe93ec5e387dc8ec6a3beb39
gillerick <ogayogill95@gmail.com>
2021-02-05 17:17:41 +0300

    More optimized and simple solution

--- Maps [Challenge].go
+++ Maps [Challenge].go
@@ -7,24 +7,12 @@ import (
 
 func main() {
 	text := "Needles and pins Needles and pins Sew me a sail to catch me the wind"
-	split_text := strings.Fields(text)
+	words := strings.Fields(text)
 	//Creating an empty memo map to store words, as keys
-	memo := map[string]int{}
+	counts := map[string]int{}
 
-	for i := 0; i < len(split_text); i++ {
-		memo[split_text[i]] = 0
-		//fmt.Printf("Text: %v Type (%T)", split_text[i], text[i])
+	for _, word := range words {
+		counts[strings.ToLower(word)]++
 	}
-
-	for j := 0; j < len(split_text); j++ {
-		_, ok := memo[split_text[j]]
-		if ok {
-			memo[split_text[j]] += 1
-		}
-		//fmt.Printf("Text: %v Type (%T)", split_text[i], text[i])
-	}
-	//fmt.Println(count)
-	for k, v := range memo {
-		fmt.Println(k, v)
-	}
+	fmt.Println(counts)
 }
