3f485e8e3843cd701e41ff711717eb16409cd593
gillerick <ogayogill95@gmail.com>
2021-02-05 16:30:31 +0300

    Added double-context method

--- Maximal value [Challenge].go
+++ Maximal value [Challenge].go
@@ -6,6 +6,7 @@ func main() {
 	nums := []int{16, 8, 42, 4, 23, 15}
 	//Begin with the assumption that the first item is the largest
 	max := nums[0]
+	//Use double-context to get the values and the indices
 	for _, value := range nums[1:] {
 		if value > max {
 			max = value
